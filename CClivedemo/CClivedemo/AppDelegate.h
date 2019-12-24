118c456f906ca0240f6c95518778c0b54e289f36
Eonlychen <eonly_chen@163.com>
2019-12-24 10:05:14 +0800

    基于cocoapods集成LiveVideoCoreSDK

---
+++ CClivedemo/CClivedemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  CClivedemo
+//
+//  Created by guxiangyun on 2019/12/23.
+//  Copyright © 2019 chometown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ CClivedemo/CClivedemo/AppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.m
+//  CClivedemo
+//
+//  Created by guxiangyun on 2019/12/23.
+//  Copyright © 2019 chometown. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+@end
